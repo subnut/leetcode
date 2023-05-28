a42df6e9618f0c09bef6e6f043ec04aacd693402
Subhaditya Nath <sn03.general@gmail.com>
2023-05-28 18:51:21 +0530

    [UPD] 412 - Fizz Buzz (C)

--- 412.c
+++ 412.c
@@ -1,15 +1,6 @@
-#ifndef DEBUG
-#define DEBUG 0
-#endif
-
 #include <stdio.h>
 #include <stdlib.h>
-
-#if DEBUG
-#define DPRINTF printf
-#else
-#define DPRINTF(...) {;}
-#endif
+#include <string.h>
 
 /*
  * NOTE: The returned array must be malloced, assume caller calls free()
@@ -17,39 +8,40 @@
 
 #define fizzBuzz fizzbuzz
 
-char *F = "Fizz";
-char *B = "Buzz";
-char *FB = "FizzBuzz";
+static void Fizz	(char **str) { memcpy(*str = calloc(5, sizeof (char)), "Fizz"	, 5); }
+static void Buzz	(char **str) { memcpy(*str = calloc(5, sizeof (char)), "Buzz"	, 5); }
+static void FizzBuzz	(char **str) { memcpy(*str = calloc(9, sizeof (char)), "FizzBuzz"	, 9); }
+static void Self	(char **str, int n)
+{
+	char len = 2;	// for trailing NUL
+	if (n > 9)	len++;
+	if (n > 99)	len++;
+	if (n > 999)	len++;
+	if (n > 9999)	len++;
+	/* We know that 0 < n <= 10000 */
+	sprintf(*str = calloc(len, sizeof (char)), "%d", n);
+}
 
 char **
 fizzbuzz(int n, int *return_size)
 {
 	*return_size = n;
-	char **result;
 
-	// Assuming malloc never fails
-	result = malloc((sizeof (char *)) * n);
+	char **result;
+	result = calloc(n, sizeof (char *));
 
 	int N;
 	char *p;
 	char flag;
-	DPRINTF("\ni\tN\tflag\n");
+
 	for (int i = 0; i < n; i++) {
 		N = i+1;
 		flag = (N % 3 == 0) + 2 * (N % 5 == 0);
-		DPRINTF("%d\t%d\t%d\n", i, N, flag);
 		switch (flag) {
-			case 0:
-				DPRINTF("\tneed %d characters\n", snprintf(NULL, 0, "%d", N));
-				DPRINTF("\tallocating %d bytes\n", (sizeof (char)) * (snprintf(NULL, 0, "%d", N) + 1));
-				result[i] = malloc((sizeof (char)) * (snprintf(NULL, 0, "%d", N) + 1));
-				DPRINTF("\tallocated %d bytes\n",  (sizeof (char)) * (snprintf(NULL, 0, "%d", N) + 1));
-				sprintf(result[i], "%d", N);
-				DPRINTF("\t\"%s\"\n", result[i]);
-				continue;
-			case 1: result[i] = F;	continue;
-			case 2: result[i] = B;	continue;
-			case 3: result[i] = FB;	continue;
+			case 0: Self(&result[i], N);	continue;
+			case 1: Fizz(&result[i]);	continue;
+			case 2: Buzz(&result[i]);	continue;
+			case 3: FizzBuzz(&result[i]);	continue;
 		}
 	}
 
@@ -60,7 +52,6 @@ fizzbuzz(int n, int *return_size)
 int main(void) {
 	int buf;
 	char **res = fizzBuzz(17, &buf);
-	DPRINTF("\ni\ttext\n");
 	for (int i = 0; i < buf; i++)
 		printf("%d\t\"%s\"\n", i, res[i]);
 	return 0;
